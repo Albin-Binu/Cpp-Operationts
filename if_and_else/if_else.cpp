a31515bcc59cf10783ccb3ebc08f795f4f747067
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2021-11-20 08:59:11 +0530

    Update and rename A to if_else.cpp

---
+++ if_and_else/if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+
+{
+
+ cout<<"enter your Score here"<<endl;
+ int score;
+ cin>>score;
+ if (score>=80) 
+ cout<<"You are passed! And your grade is A+"<<endl;
+ else if (score>60)
+ cout<<"you are passed! And your grade is B+"<<endl;
+ else if (score>=40)
+ cout<<"You are passed! And your grade is C+"<<endl;
+ else (score>=20)
+ cout<<"you are passed! And your grade is D+"<<endl;
+ return 0;
+
+}
